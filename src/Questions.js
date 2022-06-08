43df565dfb5bde7b5ddc66bcd581f7125061a659
Muzammil <shanikbs6242@gmail.com>
2022-06-08 19:11:19 +0500

    new changes

---
+++ src/Questions.js
@@ -0,0 +1,156 @@
+const questions = [
+    {
+        questionText: 'This one is condensation polymer:',
+        answerOptions: [
+            { answerText: 'PVC', isCorrect: false },
+            { answerText: 'PVA', isCorrect: false },
+            { answerText: 'Polythylene', isCorrect: false },
+            { answerText: 'Terylene', isCorrect: true },
+        ],
+    },
+    {
+        questionText: 'This number of isomers of pantane is :',
+        answerOptions: [
+            { answerText: '3', isCorrect: true },
+            { answerText: '5', isCorrect: false },
+            { answerText: '7', isCorrect: false },
+            { answerText: '9', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'Propanone and propanal are',
+        answerOptions: [
+            { answerText: 'metamers', isCorrect: false },
+            { answerText: 'chain isomers', isCorrect: false },
+            { answerText: 'Functional Group Isomers', isCorrect: true },
+            { answerText: 'position isomers', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'This is not a nuclephile',
+        answerOptions: [
+            { answerText: 'OH', isCorrect: false },
+            { answerText: 'NH3', isCorrect: false },
+            { answerText: 'BF3', isCorrect: true },
+            { answerText: 'CN', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'Reduction of aldehyde gives',
+        answerOptions: [
+            { answerText: 'Carboxylic acid', isCorrect: false },
+            { answerText: 'Alcohol', isCorrect: true },
+            { answerText: 'Ester', isCorrect: false },
+            { answerText: 'Ether', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'Hyberdization in the carbon atom of carbonyl group is of this type',
+        answerOptions: [
+            { answerText: 'sp', isCorrect: false },
+            { answerText: 'sp2', isCorrect: true },
+            { answerText: 'sp3', isCorrect: false },
+            { answerText: 'dsp2', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'This imaprts green colour to glass',
+        answerOptions: [
+            { answerText: 'CoO', isCorrect: false },
+            { answerText: 'MnO2', isCorrect: false },
+            { answerText: 'CuO', isCorrect: false },
+            { answerText: 'Cr2O3', isCorrect: true },
+        ],
+    },
+    {
+        questionText: 'Rickets is caused by the deficiency of Vitamin:',
+        answerOptions: [
+            { answerText: 'A', isCorrect: false },
+            { answerText: 'B', isCorrect: false },
+            { answerText: 'C', isCorrect: false },
+            { answerText: 'D', isCorrect: true },
+        ],
+    },
+    {
+        questionText: 'Milk Suger is also called:',
+        answerOptions: [
+            { answerText: 'Glucose', isCorrect: false },
+            { answerText: 'Fructose', isCorrect: false },
+            { answerText: 'Lactose', isCorrect: true },
+            { answerText: 'Sucrose', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'Octate rule fulfiled by all nobel gases except',
+        answerOptions: [
+            { answerText: 'Ne', isCorrect: false },
+            { answerText: 'Ar', isCorrect: false },
+            { answerText: 'He', isCorrect: true },
+            { answerText: 'Xe', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'The element with atomic number 33 belongs to',
+        answerOptions: [
+            { answerText: 's-block', isCorrect: false },
+            { answerText: 'p-block', isCorrect: true },
+            { answerText: 'd-block', isCorrect: false },
+            { answerText: 'f-block', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'Hydrides of VA group are',
+        answerOptions: [
+            { answerText: 'basic', isCorrect: true },
+            { answerText: 'acidic', isCorrect: false },
+            { answerText: 'amphoteric', isCorrect: false },
+            { answerText: 'neutral', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'The nucleus of Tritinium consist of',
+        answerOptions: [
+            { answerText: 'one person only', isCorrect: false },
+            { answerText: 'One proton and one neutron', isCorrect: false },
+            { answerText: 'two proton and two neutron', isCorrect: false},
+            { answerText: 'one proton and two neutron', isCorrect: true },
+        ],
+    },
+    {
+        questionText: 'The metal form superOxide',
+        answerOptions: [
+            { answerText: 'Li', isCorrect: false },
+            { answerText: 'Be', isCorrect: false },
+            { answerText: 'K', isCorrect: true},
+            { answerText: 'Mg', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'Antidote of H2S poisoning is very dilute: ',
+        answerOptions: [
+            { answerText: 'Nitrogen', isCorrect: false },
+            { answerText: 'Oxygen', isCorrect: false },
+            { answerText: 'chlorine', isCorrect: true},
+            { answerText: 'Helium', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'The product of heating Boric acid to 140C is ',
+        answerOptions: [
+            { answerText: 'Orthoboric Acid', isCorrect: false },
+            { answerText: 'Metaboric', isCorrect: false },
+            { answerText: 'Pyroboric', isCorrect: true},
+            { answerText: 'Boric Oxide', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'Galvanized iron mean iron coated with ',
+        answerOptions: [
+            { answerText: 'Zn', isCorrect: true },
+            { answerText: 'Cu', isCorrect: false },
+            { answerText: 'Al', isCorrect: false},
+            { answerText: 'Sa', isCorrect: false },
+        ],
+    },
+];
+export default questions
